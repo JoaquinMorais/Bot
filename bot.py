f9c96f2c652a7991584b6e13fce97bd361d0ba0e
unknown <joaquinmorais2005@gmail.com>
2022-04-01 20:42:18 -0300

    first commit

---
+++ bot.py
@@ -0,0 +1,126 @@
+import click
+import pyautogui as pg
+import time
+import random
+
+tiempo = 0.001
+cantidad = 5
+numTelefono = "+54 9 3516 43-7630"
+texto = "Puto"
+
+
+barraDeWts = 834,989
+
+def abrir(pos,click=1):
+    pg.moveTo(pos)
+    pg.click(clicks=click)
+
+
+def Main(n):
+    Hacker()
+    time.sleep(5)
+    Inicio()
+    BraveRick(1)
+    CosasRandom(n)
+
+def Exploradores():
+    Inicio()
+    ExploradorDeArchivos()
+    NavegadorChorme()
+    NavegadorOpera()
+    NavegadorBrave()
+    NavegadorFirefox()
+def Hacker():
+    pg.hotkey('win','r')
+    pg.typewrite('cmd\n',0.1)
+    pg.typewrite('color a\n',0.01)
+    pg.typewrite('tree\n',0.01)
+    pg.typewrite('tree\n',0.01)
+    pg.hotkey('win','t')
+    
+    
+
+def Inicio():
+    pg.hotkey('win', 'r')
+    pg.typewrite('notepad\n')
+    pg.typewrite('Ahora tienes un virus :)\n',0.1)
+    time.sleep(1)
+def ExploradorDeArchivos():
+    pg.hotkey('win','e')
+    pg.hotkey('win','t')
+    time.sleep(0.1)
+
+def NavegadorChorme():
+    pg.hotkey('win','r')
+    pg.typewrite('chrome\n',tiempo)
+    pg.hotkey('win','t')
+
+def NavegadorFirefox():
+    pg.hotkey('win','r')
+    pg.typewrite('firefox\n',tiempo)
+    pg.hotkey('win','t')
+
+
+def NavegadorOpera():
+    pg.hotkey('win','r')
+    pg.typewrite('opera\n',tiempo)
+    pg.hotkey('win','t')
+
+
+def NavegadorBrave():
+    pg.hotkey('win','r')
+    pg.typewrite('brave\n',tiempo)
+    pg.hotkey('win','t')
+
+def CosasRandom(n):
+    for i in range(0,n):
+        numero = random.randint(1,5)
+        if numero == 1:
+            NavegadorBrave()
+        if numero == 2:
+            NavegadorChorme()
+        if numero == 3:
+            NavegadorFirefox()
+        if numero == 4:
+            NavegadorOpera()
+        if numero == 5:
+            ExploradorDeArchivos()
+
+def BraveRick(n):
+    for i in range(0,n):
+        pg.hotkey('win','r')
+        pg.typewrite('brave\n',tiempo)
+        pg.typewrite("https://youtu.be/mCdA4bJAGGk\n",0.01)
+
+
+def MuchosExploradores(n):
+    for i in range(0,n):
+        ExploradorDeArchivos()
+        
+
+def WhatsApp(n):
+    pg.hotkey('win','r')
+    pg.typewrite('brave\n',tiempo)
+    pg.typewrite(f"https://web.whatsapp.com/send?phone=+{numTelefono}\n",0.001)
+    time.sleep(10)
+    abrir(barraDeWts)
+    abrir(barraDeWts)
+    
+    for i in range(0,n):
+        pg.typewrite(f"{texto}\n")
+
+def Alerta(t1,t2):
+    pg.alert("BOENAAAS")
+    
+def clicks(n):
+    time.sleep(10)
+
+    for i in range(0,n):
+        pg.click(clicks=1)
+
+
+
+
+
+
+print(pg.displayMousePosition())
---
+++ botWts.py
@@ -0,0 +1,131 @@
+from http.client import responses
+import pyautogui as pg
+import time
+import random, re
+import pyperclip as clip
+
+
+
+ub = 700,932
+barra = 700,1004
+
+"""
+time.sleep(2)
+
+
+try:
+    new_msg = pg.locateOnScreen('msg.png',confidence=.1)
+
+    if new_msg is not None:
+        pg.moveTo(new_msg)
+        pg.moveRel(-150,0)
+        pg.click()
+        time.sleep(.5)
+        print("New message detected")
+    else:
+        print("No new message detected")
+except:
+    print("Error")
+    pass
+"""
+
+def cleanMensage(msg):
+    vocals = ['a','e','i','o','u']
+    for index, item in enumerate(['á','é','í','ó','ú']):
+        if item in msg:
+            clean_txt = msg.replace(item,vocals[index])
+        else:
+            clean_txt = msg
+    return clean_txt
+
+def process_response(msg):
+    mensage = str(msg).lower()
+    mensage = cleanMensage(mensage)
+    responses = ['Ehh Mesi']
+    if '!' in mensage:
+        if mensage == '!help':
+            return "func01"
+        elif mensage == '!hola':
+            return "Hola!!! Soy un bot de prueba, escribiendo '!help' te daré una lista de comandos"
+        elif mensage == '!turnoff':
+            return "func02"
+
+    if 'hola' in mensage:
+        responses = ['Hola', 'Hola, que tal', 'Hola, como estas', 'Hola, como estas?','Tu nariz contra mis bolas']
+    elif 'vs' in mensage or 'vos' in mensage or 'como estas' in mensage or 'como andas' in mensage or 'como te encontras' in mensage or 'todo bien' in mensage or 'como te encontras' in mensage:
+        responses = ['Muy bien', 'Muy bien, gracias por preguntar.', 'Muy piola, recien naci']
+    
+     
+    return random.choice(responses)
+
+
+def getMesage():
+    pg.moveTo(ub)
+    pg.tripleClick()
+    pg.hotkey('ctrl','c')
+    msg = clip.paste()
+    pg.click()
+    print(f"mensaje recibido: {msg}")
+    return msg
+
+
+
+
+
+def escribir(texto):
+    pg.moveTo(barra)
+    pg.doubleClick()
+    pg.typewrite(f"{texto}\n")
+def listado():
+    pg.moveTo(barra)
+    pg.doubleClick()
+    pg.typewrite(f"Listado de comandos:")
+    pg.hotkey('shift','enter')
+    pg.typewrite(f"!help: Ayuda")
+    pg.hotkey('shift','enter')
+    pg.typewrite(f"!hola: Saludo")
+    pg.hotkey('shift','enter')
+    pg.typewrite(f"!turnoff: Apagar El Bot")
+    pg.hotkey('shift','enter')
+    pg.typewrite(f"\n")
+
+
+
+
+
+def prenderBot():
+    aux = 0
+    time.sleep(10)
+    escribir("Bot Encendido")
+    while True:
+        aux+=1
+        boolean = pg.pixelMatchesColor(ub[0],ub[1],(17,25,31))
+        print(boolean)
+        
+        if boolean == False:
+            print("Nuevo Mensaje")
+            respuesta = process_response(getMesage())
+            if 'func' in respuesta:
+                if '01' in respuesta:
+                    listado()
+                elif '02' in respuesta:
+                    break
+            else:
+                escribir(respuesta)
+        
+        if(aux==30):
+            break
+        time.sleep(1)
+
+    escribir("Bot Apagado...")
+
+def pruebaTexto(mensage):
+    if 'hola' in mensage:
+        return True
+    else:
+        return False
+
+
+
+time.sleep(5)
+listado()
